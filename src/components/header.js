d14e6ea7b4b7f04da82ed209d6563306a4efae26
mjplata <mjplata99@gmail.com>
2023-07-14 17:56:28 -0500

    Fixes

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,10 @@
 import { Button } from 'antd';
-import { LogoutOutlined } from '@ant-design/icons';
 import './header.css'
 
 function Header () {
     return (
         <div className="header"> 
             <div className="logout-container">
-                <span>
-                    <LogoutOutlined />
-                </span>
                 <Button size="large">Cerrar sesión</Button>
             </div>
         </div>
